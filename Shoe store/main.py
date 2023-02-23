0aa6135d087f538dbbf9f677953e242060e53aa7
BaileyPhillips <126138686+BaileyPhillips@users.noreply.github.com>
2023-02-23 19:34:04 +1300

    Version 0.3.2 - Removed Code From 0.3
    Removed the code that was meant to be replace in version 0.3.1

--- Shoe store/main.py
+++ Shoe store/main.py
@@ -3,23 +3,6 @@ print("At Bailey's Shoes, we strive to help you find the "
       "perfect shoes for your style, whether you're looking"
       " for running shoes, sports shoes, or just a comfortable pair of everyday shoes.")
 
-print("Which type of shoes are you looking for?")
-print("1. Running Shoes")
-print("2. Sports Shoes")
-print("3. Everyday Shoes")
-
-choice = input("Press the button of the type of shoes you are looking for: ")
-
-if choice == "1":
-    print("You have selected Running Shoes. You will now be redirected to the Running Shoes page")
-    print("You have been redirected to the Running Shoes page. Here you will find a range of running shoes to choose from.")
-elif choice == "2":
-    print("You have selected Sports Shoes. You will now be redirected to the Sports Shoes page")
-    print("You have been redirected to the Sports Shoes page. Here you will find a range of sports shoes to choose from.")
-elif choice == "3":
-    print("You have selected Everyday Shoes. You will now be redirected to the Everyday Shoes page")
-    print("You have been redirected to the Everyday Shoes page. Here you will find a range of Everyday Shoes to choose from.")
-
 RUNNING_SHOES = "Running Shoes"
 SPORTS_SHOES = "Sports Shoes"
 EVERYDAY_SHOES = "Everday Shoes"
@@ -40,4 +23,6 @@ elif choice == "3":
     shoe_type = EVERYDAY_SHOES
 
 print(f"You have selected {shoe_type}. You will now be redirected to the {shoe_type} page")
-print(f"You have been redirected to the {shoe_type} page. Here you will find a range of {shoe_type} to choose from.")
+print(f"You have been redirected to the {shoe_type} page. "
+      f"Here you will find a range of {shoe_type} to choose from.")
+
