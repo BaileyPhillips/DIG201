dbc11c1ff5807d8dbba83e722b01fc4a048a9b1c
BaileyPhillips <126138686+BaileyPhillips@users.noreply.github.com>
2023-02-23 20:59:38 +1300

    Version 1.2 - Added Product
    added product class

---
+++ Shoe store/model/product.py
@@ -0,0 +1,18 @@
+class Product:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+
+    def get_name(self):
+        return self._name
+
+    def get_price(self):
+        return self._price
+
+    def get_price_string(self):
+        return "${:0.2f}".format(self._price)
+
+    def __str__(self):
+        return f"{self._name} {self.get_price_string()}"
+
+    
