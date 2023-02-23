8c4a04370a53b7d658abba3146d966832a225550
BaileyPhillips <126138686+BaileyPhillips@users.noreply.github.com>
2023-02-23 19:10:37 +1300

    Version 0.2 - Added Description
    Added a description to the welcome message

--- Shoe store/main.py
+++ Shoe store/main.py
@@ -1,1 +1,4 @@
 print("Welcome to Bailey's Shoes!")
+print("At Bailey's Shoes, we strive to help you find the "
+      "perfect shoes for your style, whether you're looking"
+      " for running shoes, sports shoes, or just a comfortable pair of everyday shoes.")
