0b6ac8030caa2643a265644c584f7b414ff16126
BaileyPhillips <126138686+BaileyPhillips@users.noreply.github.com>
2023-02-23 19:18:55 +1300

    Version 0.3 - Choose Shoe Type
    Added user input for choosing the type of shoe

--- Shoe store/main.py
+++ Shoe store/main.py
@@ -2,3 +2,21 @@ print("Welcome to Bailey's Shoes!")
 print("At Bailey's Shoes, we strive to help you find the "
       "perfect shoes for your style, whether you're looking"
       " for running shoes, sports shoes, or just a comfortable pair of everyday shoes.")
+
+print("Which type of shoes are you looking for?")
+print("1. Running Shoes")
+print("2. Sports Shoes")
+print("3. Everyday Shoes")
+
+choice = input("Press the button of the type of shoes you are looking for: ")
+
+if choice == "1":
+    print("You have selected Running Shoes. You will now be redirected to the Running Shoes page")
+    print("You have been redirected to the Running Shoes page. Here you will find a range of running shoes to choose from.")
+elif choice == "2":
+    print("You have selected Sports Shoes. You will now be redirected to the Sports Shoes page")
+    print("You have been redirected to the Sports Shoes page. Here you will find a range of sports shoes to choose from.")
+elif choice == "3":
+    print("You have selected Everyday Shoes. You will now be redirected to the Everyday Shoes page")
+    print("You have been redirected to the Everyday Shoes page. Here you will find a range of Everyday Shoes to choose from.")
+
